GoBeromsu/obsidian-eagle-plugin

commit 69ea560f57543e19c68b59e51c6d407c3b141417

fix: fix 'File' type for images coming from "new window" notes
Fixes #68

src/ImgurPlugin.ts
@@ -12,6 +12,7 @@ import ImgurSize from './imgur/resizing/ImgurSize'
 import AuthenticatedImgurClient from './imgur/AuthenticatedImgurClient'
 import ImgurAuthenticatedUploader from './uploader/imgur/ImgurAuthenticatedUploader'
 import { allFilesAreImages } from './utils/FileList'
+import { fixImageTypeIfNeeded } from './utils/misc'
 import { createImgurCanvasPasteHandler } from './Canvas'
 
 declare module 'obsidian' {
@@ -191,7 +192,11 @@ export default class ImgurPlugin extends Plugin {
   }
 
   setupImagesUploader(): void {
-    this.imgUploaderField = buildUploaderFrom(this.settings)
+    const uploader = buildUploaderFrom(this.settings)
+    this.imgUploaderField = {
+      upload: (image: File, albumId?: string) =>
+        uploader.upload(fixImageTypeIfNeeded(image), albumId),
+    }
   }
 
   private setupImgurHandlers() {

src/utils/misc.ts
@@ -1,0 +1,12 @@
+function fixImageTypeIfNeeded(image: File) {
+  if (passesInstanceofCheck(image)) {
+    return image
+  }
+  return new File([image], image.name, { type: image.type, lastModified: image.lastModified })
+
+  function passesInstanceofCheck(image: File): boolean {
+    return image instanceof File
+  }
+}
+
+export { fixImageTypeIfNeeded }

commit 07367d180f4ab24e370321420da6d6248b66c444

fix: fix broken authentication
Fixes #72
Was broken by 69ea560f57543e19c68b59e51c6d407c3b141417

src/ImgurPlugin.ts
@@ -193,9 +193,14 @@ export default class ImgurPlugin extends Plugin {
 
   setupImagesUploader(): void {
     const uploader = buildUploaderFrom(this.settings)
-    this.imgUploaderField = {
-      upload: (image: File, albumId?: string) =>
-        uploader.upload(fixImageTypeIfNeeded(image), albumId),
+    this.imgUploaderField = uploader
+    if (!uploader) return
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    const originalUploadFunction = uploader.upload
+    uploader.upload = function (image: File, albumId?: string) {
+      if (!uploader) return
+      return originalUploadFunction.call(uploader, fixImageTypeIfNeeded(image), albumId)
     }
   }
 
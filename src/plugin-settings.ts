448d7795ec9bfceae907ee14be0c8d45b018028c
Beomsu Koh <gobeumsu@gmail.com>
2025-11-10 21:09:33 +0000

    feat: add eagleFolderName setting and folder management in EagleUploader
    - Introduced eagleFolderName setting to specify the folder for saving images in Eagle
    - Enhanced EagleUploader with methods to list and create folders in Eagle
    - Updated upload method to associate images with the specified folder, if provided

--- src/plugin-settings.ts
+++ src/plugin-settings.ts
@@ -1,9 +1,11 @@
 export interface EaglePluginSettings {
   eagleHost: string
   eaglePort: number
+  eagleFolderName: string
 }
 
 export const DEFAULT_SETTINGS: EaglePluginSettings = {
   eagleHost: 'localhost',
   eaglePort: 41595,
+  eagleFolderName: '',
 }
